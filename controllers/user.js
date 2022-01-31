0076cce84a37a43fdf562afcff6b3d8ede43cbe9
GuillaumeDavid23 <guillaume.david744@orange.fr>
2022-01-31 14:29:48 +0100

    Find all

--- controllers/user.js
+++ controllers/user.js
@@ -28,4 +28,18 @@ const getOne = async (req, res) => {
     }
 }
 
-export  {getOne, create}
+const getAll = async (req, res) => {        
+    try {
+        const user = await User.find()
+        if (user){
+            res.status(200).json(user)
+        }else{
+            res.status(204).json({message: 'Aucun utilisateur'})
+        } 
+    } catch (error) {
+        console.log(error);
+        res.status(400).json(error)
+    }
+}
+
+export  {getOne, getAll, create}
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import {getOne, create} from '../controllers/user.js'
+import {getOne, getAll, create} from '../controllers/user.js'
 
 const router = express.Router()
 
 router.get('/:_id', getOne)
+router.get('/', getAll)
 router.post('/', create)
 
 export default router
