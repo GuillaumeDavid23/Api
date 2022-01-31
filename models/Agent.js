05a0b6e1749622d1cf80f147dede65ab0bd532aa
Benjamin RAULT <b_rault@outlook.fr>
2022-01-31 16:18:40 +0100

    Try to extends Schema from UserModel

--- models/Agent.js
+++ models/Agent.js
@@ -1,7 +1,36 @@
 import Mongoose from '../db/db.js'
+import extend from 'mongoose-schema-extend'
 
-const agentSchema = Mongoose.Schema({
+const userShema = Mongoose.Schema(
+	{
+		firstname: { type: String, required: true },
+		lastname: { type: String, required: true },
+		email: { type: String, required: true, unique: true },
+		password: { type: String, required: true },
+		token: { type: String, required: false },
+		phone: { type: String, required: false },
+		newsletter: { type: String, required: false },
+		status: { type: String, required: true },
+		ref: { type: String, required: false },
+	},
+	{ collection: 'users' }
+)
+
+const agentSchema = userShema.extend({
 	pro_phone_number: { type: String, required: true },
 })
 
-export default Mongoose.model('Agent', agentSchema)
+const Agent = Mongoose.model('Agent', agentSchema)
+
+const Ag = new Agent({
+	firstname: 'Benjamin',
+	lastname: 'RAULT',
+	email: 'agent@agent.com',
+	password: 'h4k3r',
+	status: 'crepe',
+	pro_phone_number: '0000000000',
+})
+
+console.log(Ag)
+
+export default Agent
