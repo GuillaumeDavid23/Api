e350ff7a5f5a8383334c59e2ece2fbb901755d81
Vincent Mancheron <vmancheron@yahoo.fr>
2022-02-01 09:34:21 +0100

    01/02/22 09:34

--- controllers/buyer.js
+++ controllers/buyer.js
@@ -1,3 +1,27 @@
 import Buyer from './Buyer.js'
 
-export {}
+const getBuyer = async () => {
+	try {
+		const buyer = await Buyer.findById(req.params._id)
+		if (buyer) {
+			res.status(200).json(buyer)
+		} else {
+			res.status(204).json({ message: 'Aucun acheteur' })
+		}
+	} catch (error) {
+		console.log(error)
+		res.status(400).json(error)
+	}
+}
+
+const getAllBuyers = () => {
+	Buyer.find()
+		.then((buyers) => res.status(200).json(buyers))
+		.catch((error) =>
+			res.status(400).json({
+				error,
+			})
+		)
+}
+
+export { getAllBuyers, getBuyer }
--- controllers/role.js
+++ controllers/role.js
@@ -1,3 +1,17 @@
 import Role from './Role.js'
 
-export {}
+const getOne = async (req, res) => {
+	try {
+		const role = await Role.findById(req.params._id)
+		if (role) {
+			res.status(200).json(role)
+		} else {
+			res.status(204).json({ message: 'Aucun rôle' })
+		}
+	} catch (error) {
+		console.log(error)
+		res.status(400).json(error)
+	}
+}
+
+export { getOne }
