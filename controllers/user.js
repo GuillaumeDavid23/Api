692c3019e5f40bfc15b331d117a0044ad4c24c6f
GuillaumeDavid23 <guillaume.david744@orange.fr>
2022-02-01 09:37:16 +0100

    forgotPass method

--- controllers/user.js
+++ controllers/user.js
@@ -1,23 +1,28 @@
 import User from '../models/User.js'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+
 const create = (req, res) => {
-    const saltRounds = 10;
-    delete req.body._id;
-    const user = new User({
-        ...req.body,
-    });
-    bcrypt.hash(user.password, saltRounds, function(err, hash) {
-        user.password = hash
-        user.save()
-            .then(() => res.status(201).json({
-                message: 'Utilisateur créé !'
-            }))
-            .catch(error => res.status(400).json({
-                error
-            }));
-    });
-};
+	const saltRounds = 10
+	delete req.body._id
+	const user = new User({
+		...req.body,
+	})
+	bcrypt.hash(user.password, saltRounds, function (err, hash) {
+		user.password = hash
+		user.save()
+			.then(() =>
+				res.status(201).json({
+					message: 'Utilisateur créé !',
+				})
+			)
+			.catch((error) =>
+				res.status(400).json({
+					error,
+				})
+			)
+	})
+}
 
 const update = (req, res) => {
 	User.updateOne(
@@ -28,51 +33,48 @@ const update = (req, res) => {
 			...req.body,
 		}
 	)
-		.then((response) =>{
+		.then((response) => {
 			res.status(201).json({
 				message: 'Utilisateur modifié !',
 			})
-        }
-            
-		)
+		})
 		.catch((error) =>
 			res.status(400).json({
 				error,
 			})
 		)
-    
 }
 
-const getOne = async (req, res) => {        
-    try {
-        const user = await User.findById(req.params._id)
-        if (user){
-            res.status(200).json(user)
-        }else{
-            res.status(204).json({message: 'Aucun utilisateur'})
-        } 
-    } catch (error) {
-        console.log(error);
-        res.status(400).json(error)
-    }
+const getOne = async (req, res) => {
+	try {
+		const user = await User.findById(req.params._id)
+		if (user) {
+			res.status(200).json(user)
+		} else {
+			res.status(204).json({ message: 'Aucun utilisateur' })
+		}
+	} catch (error) {
+		console.log(error)
+		res.status(400).json(error)
+	}
 }
 
-const getAll = async (req, res) => {        
-    try {
-        const user = await User.find()
-        if (user){
-            res.status(200).json(user)
-        }else{
-            res.status(204).json({message: 'Aucun utilisateur'})
-        } 
-    } catch (error) {
-        console.log(error);
-        res.status(400).json(error)
-    }
+const getAll = async (req, res) => {
+	try {
+		const user = await User.find()
+		if (user) {
+			res.status(200).json(user)
+		} else {
+			res.status(204).json({ message: 'Aucun utilisateur' })
+		}
+	} catch (error) {
+		console.log(error)
+		res.status(400).json(error)
+	}
 }
 
 const deleteOne = async (req, res) => {
-    User.updateOne(
+	User.updateOne(
 		{
 			_id: req.params._id,
 		},
@@ -80,13 +82,11 @@ const deleteOne = async (req, res) => {
 			status: 0,
 		}
 	)
-		.then((response) =>{
+		.then((response) => {
 			res.status(201).json({
 				message: 'Utilisateur désactivé !',
 			})
-        }
-            
-		)
+		})
 		.catch((error) =>
 			res.status(400).json({
 				error,
@@ -95,58 +95,90 @@ const deleteOne = async (req, res) => {
 }
 
 const signup = async (req, res) => {
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User({
-                ...req.body,
-                password: hash
-            });
-            user.save()
-                .then(() => res.status(201).json({
-                    message: 'Utilisateur créé !'
-                }))
-                .catch(error => res.status(400).json({
-                    error
-                }))
-        })
-        .catch(error => res.status(500).json({
-            error
-        }))
-};
+	bcrypt
+		.hash(req.body.password, 10)
+		.then((hash) => {
+			const user = new User({
+				...req.body,
+				password: hash,
+			})
+			user.save()
+				.then(() =>
+					res.status(201).json({
+						message: 'Utilisateur créé !',
+					})
+				)
+				.catch((error) =>
+					res.status(400).json({
+						error,
+					})
+				)
+		})
+		.catch((error) =>
+			res.status(500).json({
+				error,
+			})
+		)
+}
 
 const login = async (req, res) => {
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({
-                    error: 'Utilisateur non trouvé !'
-                });
-            }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if (!valid) {
-                        return res.status(401).json({
-                            error: 'Mot de passe incorrect !'
-                        });
-                    }
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '24h' }
-                        ),
-                        message: 'Utilisateur connecté !'
+	User.findOne({ email: req.body.email })
+		.then((user) => {
+			if (!user) {
+				return res.status(401).json({
+					error: 'Utilisateur non trouvé !',
+				})
+			}
+			bcrypt
+				.compare(req.body.password, user.password)
+				.then((valid) => {
+					if (!valid) {
+						return res.status(401).json({
+							error: 'Mot de passe incorrect !',
+						})
+					}
+					res.status(200).json({
+						userId: user._id,
+						token: jwt.sign(
+							{ userId: user._id },
+							'RANDOM_TOKEN_SECRET',
+							{ expiresIn: '24h' }
+						),
+						message: 'Utilisateur connecté !',
+					})
+				})
+				.catch((error) =>
+					res.status(500).json({
+						error,
+					})
+				)
+		})
+		.catch((error) =>
+			res.status(500).json({
+				error,
+			})
+		)
+}
 
-                    });
-                })
-                .catch(error => res.status(500).json({
-                    error
-                }))
-        })
-        .catch(error => res.status(500).json({
-            error
-        }));
-};
+const forgotPass = async (req, res) => {
+	User.findOne({ email: req.body.email })
+		.then((user) => {
+			User.updateOne(
+				{ _id: user.id },
+				{
+					token: jwt.sign(
+						{ userId: user._id },
+						'RANDOM_TOKEN_SECRET',
+						{ expiresIn: '5h' }
+					),
+				}
+			)
+		})
+		.catch((error) =>
+			res.status(500).json({
+				error,
+			})
+		)
+}
 
-export  {getOne, getAll, create, update, deleteOne, login, signup}
+export { getOne, getAll, create, update, deleteOne, login, signup, forgotPass }
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,20 @@
 import express from 'express'
-import {getOne, getAll, create, update, deleteOne, login, signup} from '../controllers/user.js'
+import {
+	getOne,
+	getAll,
+	create,
+	update,
+	deleteOne,
+	login,
+	signup,
+	forgotPass,
+} from '../controllers/user.js'
 
 const router = express.Router()
 
 router.post('/login', login)
 router.post('/signup', signup)
+router.post('/forgot', forgotPass)
 
 router.post('/', create)
 router.put('/:_id', update)
