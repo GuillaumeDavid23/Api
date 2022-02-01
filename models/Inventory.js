cef29c2c20ac1e9a38ca1d5b67a70bfee58dd264
Vincent Mancheron <vmancheron@yahoo.fr>
2022-02-01 09:10:49 +0100

    01/02/22 09:10

--- models/Inventory.js
+++ models/Inventory.js
@@ -1,6 +1,12 @@
 import mongoose from '../db/db.js'
+import Agent from './Agent.js'
 
 const inventoryShema = mongoose.Schema({
+	id_agent: {
+		type: Schema.type.ObjectId,
+		ref: Agent,
+		required: true,
+	},
 	inOut: { type: Boolean, required: true },
 	userReference: { type: String, required: true },
 	date: { type: Date, required: true },
--- routes/rental.js
+++ routes/rental.js
@@ -1,13 +1,13 @@
 import express from 'express'
-import stuffCtrl from '../controllers/stuff'
-import auth from '../middleware/auth'
+import { create, update, erase, getAll, getOne } from '../controllers/rental.js'
+// import auth from '../middleware/auth'
 
 const router = express.Router()
 
-router.post('/', auth, stuffCtrl.createThing)
-router.put('/:id', auth, stuffCtrl.modifyThing)
-router.delete('/:id', auth, stuffCtrl.deleteThing)
-router.get('/:id', auth, stuffCtrl.getOneThing)
-router.get('/', auth, stuffCtrl.getAllThings)
+router.post('/', auth, create)
+router.put('/:id', auth, update)
+router.delete('/:id', auth, erase)
+router.get('/:id', auth, getOne)
+router.get('/', auth, getAll)
 
 module.exports = router
