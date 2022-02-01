5e31e21cd0c93871a2658054430b25e28963ec96
GuillaumeDavid23 <guillaume.david744@orange.fr>
2022-02-01 10:12:47 +0100

    selller

--- controllers/seller.js
+++ controllers/seller.js
@@ -1,3 +1,31 @@
 import Seller from '../models/Seller.js'
 
-export {}
+const getOne = async (req, res) => {
+	try {
+		const user = await User.findById(req.params._id)
+		if (user) {
+			res.status(200).json(user)
+		} else {
+			res.status(204).json({ message: 'Aucun utilisateur' })
+		}
+	} catch (error) {
+		console.log(error)
+		res.status(400).json(error)
+	}
+}
+
+const getAll = async (req, res) => {
+	try {
+		const user = await User.find()
+		if (user) {
+			res.status(200).json(user)
+		} else {
+			res.status(204).json({ message: 'Aucun utilisateur' })
+		}
+	} catch (error) {
+		console.log(error)
+		res.status(400).json(error)
+	}
+}
+
+export { getOne, getAll }
