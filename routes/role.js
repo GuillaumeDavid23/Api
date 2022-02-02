07d053d1be1cc45544d0c42ba6a4a88f37006c5a
GuillaumeDavid23 <guillaume.david744@orange.fr>
2022-02-02 16:42:03 +0100

    Role doc

--- routes/role.js
+++ routes/role.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { getOne } from '../controllers/rental.js'
+import { getOne, getAll } from '../controllers/rental.js'
 import auth from '../middleware/auth.js'
 // import validation from '../validation/role.js'
 
 const router = express.Router()
 
 router.get('/:id', auth, getOne)
+router.get('/', auth, getAll)
 
 export default router
