2e8f1432522fd334c0b6980cc7fab182cc174aef
GuillaumeDavid23 <guillaume.david744@orange.fr>
2022-02-18 09:10:37 +0100

    comment property

--- routes/property.js
+++ routes/property.js
@@ -3,7 +3,6 @@ import * as PC from '../controllers/property.js'
 import auth from '../middleware/auth.js'
 import checkAccess from '../middleware/checkAccess.js'
 import {
-	test,
 	checkPropertyBody,
 	checkPropertyExistence,
 } from '../middleware/validation/property.js'
@@ -14,6 +13,7 @@ import {
 import multer from '../middleware/multer-config.js'
 
 const router = express.Router()
+
 //(Update) Ajout d'un équipement dans lst_roomDetails
 router.put('/equipment/:_id', auth, checkAccess(['agent']), PC.addEquipment)
 
@@ -31,6 +31,7 @@ router.put('/heater/:_id', auth, checkAccess(['agent']), PC.addHeater)
 //(Delete) Suppression d'un équipement dans lst_roomDetails
 router.delete('/heater/:_id', auth, checkAccess(['agent']), PC.removeHeater)
 
+//(Create) Création d'une propriété
 router.post(
 	'/',
 	auth,
@@ -39,7 +40,11 @@ router.post(
 	validation,
 	PC.createProperty
 )
+
+//(Get) Récupération de toutes les propriété
 router.get('/', PC.getAllProperties)
+
+//(Get) Récupération d'une propriété
 router.get('/:_id', validateParamId(), validation, PC.getPropertyById)
 router.put(
 	'/:_id',
@@ -50,6 +55,8 @@ router.put(
 	validation,
 	PC.updateProperty
 )
+
+//(Delete) Suppression d'une propriété
 router.delete(
 	'/:_id',
 	auth,
